57fb8f8233e77a4ef57450537a4f8ad40f6fb670
rubiyat <rubiyat21@gmail.com>
2017-12-29 00:41:09 +0600

    Laravel Authentication #1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware'=>['auth']], function(){
+
+    Route::get('/admin', function(){
+
+        return view('admin.index');
+
+	});
+
+});
