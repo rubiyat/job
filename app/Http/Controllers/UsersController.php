0a8eaa4679707a5022a2dbc090ef0bfcfd1e9076
rubiyat <rubiyat21@gmail.com>
2018-01-24 20:48:17 +0600

    Delete User Data #15

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -90,8 +90,9 @@ class UsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+        return redirect('admin/users');
     }
 }
