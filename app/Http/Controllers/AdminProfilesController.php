f0878a746770428a4792338c7c3d39b4e04c6b69
rubiyat <rubiyat21@gmail.com>
2018-01-31 13:21:41 +0600

    Show Admin profile #42

--- app/Http/Controllers/AdminProfilesController.php
+++ app/Http/Controllers/AdminProfilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class AdminProfilesController extends Controller
 {
@@ -43,9 +44,9 @@ class AdminProfilesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        //
+        return view('admin.admin-profiles.show', compact(['user']));
     }
 
     /**
--- resources/views/admin/admin-profiles/show.blade.php
+++ resources/views/admin/admin-profiles/show.blade.php
@@ -4,30 +4,51 @@
 
     <div class="panel panel-danger panel-top">
       <div class="panel-heading">
-        <span class="heading">ROLE DETAILS</span>
+        <span class="heading">USER'S PROFILE</span>
         <a type="button" href="{{route('roles.index')}}" class="btn btn-primary pull-right hidden-print"> <i class="glyphicon glyphicon-menu-left"> </i> Go To Index</a>
         <button style="margin-right:5px;" title="Print" type="button" onclick="window.print();" class="btn btn-primary pull-right hidden-print"><i class="glyphicon glyphicon-print"></i></button>
         <div class="clearfix"></div>
       </div>
       <div class="panel-body">
-        <h2>{{ $role->name }}</h2>
-        <p>{{ $role->description }}</p>
-        <hr>
-        <div class="row">
-            <div class="col-lg-6">
-                {!! ($role->is_active == 0) ? 
-                    '<span class="label label-success">Active</span>' :
-                    '<span class="label label-danger">Inactive</span>'
-                !!}
+        <!-- Nav tabs -->
+          <ul class="nav nav-tabs" role="tablist">
+            <li role="presentation" class="active"><a href="#basicInfo" aria-controls="basicInfo" role="tab" data-toggle="tab">Basic Info</a></li>
+            <li role="presentation"><a href="#image" aria-controls="image" role="tab" data-toggle="tab">Image</a></li>
+          </ul>
+
+          <!-- Tab panes -->
+          <div class="tab-content">
+            <div role="tabpanel" class="tab-pane active" id="basicInfo">
+              <div class="panel panel-info">
+                <div class="panel-heading">{{ Auth::user()->name }}</div>
+                <div class="panel-body">
+                  <h4>First Name:</h4>
+                  <h4>Last Name:</h4>
+                  <h4>Address:</h4>
+                  <h4>Email:</h4>
+                  <h4>Phone:</h4>
+                </div>
+                <div class="panel-footer">
+                  <button class="btn btn-primary pull-right">Update Profile</button>
+                  <div class="clearfix"></div>
+                </div>
+              </div>
             </div>
-            <div class="col-lg-6 text-right hidden-print">
-                  <a type="button" href="{{ route('roles.edit', ['role' => $role->id]) }}" class="btn btn-success glyphicon glyphicon-edit" title="Edit"></a>
-                <form action="{!! action('RolesController@destroy', $role->id) !!}" method="POST" style="display: inline-block;">
-                    {{ csrf_field() }} {{ method_field('DELETE') }}
-                   <button type="submit" title="Delete" role="button" class="btn btn-danger"><i class="glyphicon glyphicon-trash" title="Delete"></i></button>
-                </form>        
+            <div role="tabpanel" class="tab-pane" id="image">
+              <div class="panel panel-info">
+                <div class="panel-heading">{{ Auth::user()->name }}</div>
+                <div class="panel-body">
+                  <div style="text-align: center;">
+                    <span><img height="250" width="250" src="http://userproplugin.com/userpro/wp-content/uploads/userpro/15531/587672f9c6375.png" alt=""></span>
+                  </div>                  
+                </div>
+                <div class="panel-footer">
+                  <button class="btn btn-primary pull-right">Update Profile</button>
+                  <div class="clearfix"></div>
+                </div>
+              </div>
             </div>
-        </div>
+          </div>
       </div>
     </div>
     
