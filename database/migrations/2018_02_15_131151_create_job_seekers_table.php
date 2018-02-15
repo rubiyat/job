48844900c2488913b21d431c18cd506b12b3d56f
rubiyat <rubiyat21@gmail.com>
2018-02-15 19:16:02 +0600

    seeker table #72

---
+++ database/migrations/2018_02_15_131151_create_job_seekers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobSeekersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seekers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable()->index();
+
+            $table->float('hourly_rate', 8, 2);
+            $table->time('work_time_start');
+            $table->time('work_time_end');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seekers');
+    }
+}
