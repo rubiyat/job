0f5bce53aebfabdf9cdb763c7f513889e8aa8889
rubiyat <rubiyat21@gmail.com>
2018-03-01 16:17:53 +0600

    skill table

---
+++ database/migrations/2018_03_01_101448_create_job_seeker_skills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobSeekerSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_seeker_skills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('job_seeker_id')->unsigned()->nullable()->index();
+            $table->string('title', 200);
+            $table->timestamps();
+            
+            $table->foreign('job_seeker_id')->references('id')->on('job_seekers')->onDelete('restrict')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_seeker_skills');
+    }
+}
